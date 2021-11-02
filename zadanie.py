fc178564721a6158e42fb719420cb0f229360cc3
Krzysztof Baca <baca.krzysztof@wp.pl>
2021-11-02 19:17:19 +0100

    add_loop

--- zadanie.py
+++ zadanie.py
@@ -3,3 +3,8 @@ shoping_list = {
   'piekarnia':['chleb','bułki','pączek'],
   'warzywniak':['marchew','seler','rukola']
 }
+
+for key in shoping_list:
+  print('ide do', key.title(), 'i kupuje tam',str(shoping_list[key]).title())
+
+sum_list=sum(len(value) for value in shoping_list.values())
