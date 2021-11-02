9187fdf9634ce7d2731021032202aff07d6b8aac
Krzysztof Baca <baca.krzysztof@wp.pl>
2021-11-02 19:21:26 +0100

    add_sum

--- zadanie.py
+++ zadanie.py
@@ -8,3 +8,5 @@ for key in shoping_list:
   print('ide do', key.title(), 'i kupuje tam',str(shoping_list[key]).title())
 
 sum_list=sum(len(value) for value in shoping_list.values())
+
+print('w sumie kupuję',sum_list,'produktów')
